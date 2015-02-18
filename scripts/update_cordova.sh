1aae891ad4ade5569d8d1f81e3f062fb64ef9ee7
Anne van Rossum <anne@dobots.nl>
2015-02-18 17:45:21 +0100

    Add update script

---
+++ scripts/update_cordova.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ..
+cordova platform update android
+
+echo "Current plugin versions"
+cordova plugin
+cordova plugin remove com.randdusing.bluetoothle
+cordova plugin add com.randdusing.bluetoothle
+cordova plugin remove org.apache.cordova.device
+cordova plugin add org.apache.cordova.device
+cordova plugin rm org.apache.cordova.dialogs
+cordova plugin add org.apache.cordova.dialogs
+
+echo "New plugin versions"
+cordova plugin
