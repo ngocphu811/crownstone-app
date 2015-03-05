c0fa080a3fd5bc5406a1325ed7098eaa9abbef3e
Dominik Egger <dominik@almende.org>
2015-03-05 15:46:16 +0100

    Revert "changed update_cordova script to use github link (registry problem)"
    This reverts commit a85a81ca45a98e5b8917ceb90f60177df0de6ffb.

--- scripts/update_cordova.sh
+++ scripts/update_cordova.sh
@@ -4,25 +4,15 @@ cordova platform add android
 cordova platform update android
 
 echo "Current plugin versions"
-# cordova plugin
-# cordova plugin remove com.randdusing.bluetoothle
-# cordova plugin add com.randdusing.bluetoothle
-# cordova plugin remove org.apache.cordova.device
-# cordova plugin add org.apache.cordova.device
-# cordova plugin rm org.apache.cordova.dialogs
-# cordova plugin add org.apache.cordova.dialogs
-# cordova plugin rm com.verso.cordova.clipboard
-# cordova plugin add com.verso.cordova.clipboard
-
 cordova plugin
 cordova plugin remove com.randdusing.bluetoothle
-cordova plugin add https://github.com/randdusing/BluetoothLE
+cordova plugin add com.randdusing.bluetoothle
 cordova plugin remove org.apache.cordova.device
-cordova plugin add https://github.com/apache/cordova-plugin-device
+cordova plugin add org.apache.cordova.device
 cordova plugin rm org.apache.cordova.dialogs
-cordova plugin add https://github.com/apache/cordova-plugin-dialogs
+cordova plugin add org.apache.cordova.dialogs
 cordova plugin rm com.verso.cordova.clipboard
-cordova plugin add https://github.com/VersoSolutions/CordovaClipboard
+cordova plugin add com.verso.cordova.clipboard
 
 echo "New plugin versions"
 cordova plugin
