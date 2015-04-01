e373607bff84b1d2e61f7af7970886774da46877
Wouter Bulten <wouterbulten@mai-ai.org>
2015-04-01 12:32:02 +0200

    Forgot to add the 'add' part

--- scripts/update_cordova.sh
+++ scripts/update_cordova.sh
@@ -6,7 +6,7 @@ cordova platform update android
 echo "Current plugin versions"
 cordova plugin
 cordova plugin remove org.apache.cordova.device-motion
-cordova plugin org.apache.cordova.device-motion
+cordova plugin add org.apache.cordova.device-motion
 cordova plugin remove com.randdusing.bluetoothle
 cordova plugin add com.randdusing.bluetoothle
 cordova plugin remove org.apache.cordova.device
